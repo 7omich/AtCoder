0062c43fd056ee7bf0e41f11bf7ab845ab257075
nb6u7 <b6u728@gmail.com>
2018-03-25 23:56:59 +0900

    Participate ABC_092

---
+++ ABC_092/B_Chocolate.rb
@@ -0,0 +1,14 @@
+n = gets.chomp.to_i
+d, x = gets.chomp.split(' ').map(&:to_i)
+
+arr = []
+(1..n).each do |i|
+  arr[i-1] = gets.chomp.to_i
+end
+
+arr.each do |i|
+  eaten = (d-1)/i + 1
+  x += eaten
+end
+
+p x
---
+++ ABC_092/C_Traveling_Plan.rb
@@ -0,0 +1,18 @@
+n = gets.chomp.to_i
+arr = gets.chomp.split(' ').map(&:to_i)
+
+all_dist = arr[0].abs
+(0..n-2).each do |i|
+  all_dist += (arr[i+1] - arr[i]).abs
+end
+all_dist += arr[n-1].abs
+
+p all_dist - (arr[0].abs + (arr[1] - arr[0]).abs) + arr[1].abs
+
+(1..n-2).each do |i|
+  minus = (arr[i+1] - arr[i]).abs + (arr[i] - arr[i-1]).abs
+  plus = (arr[i+1] - arr[i-1]).abs
+  p all_dist - minus + plus
+end
+
+p all_dist - (arr[n-1].abs + (arr[n-1] - arr[n-2]).abs) + arr[n-2].abs
