5c7d421b29a5bf8e320906bccad7f51aeb290c52
7omich <b6u728@gmail.com>
2019-01-11 01:56:17 +0900

    Solve A, B

---
+++ ABC_004/B_回転.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#define REP(i,n) for(int i=0; i<n; i++)
+using namespace std;
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+
+    char input[4][4];
+    REP(i, 4) {
+        REP(j, 4) {
+            cin >> input[i][j];
+        }
+    }
+
+    REP(i, 4) {
+        REP(j, 4) {
+            if (j != 3) {
+                cout << input[3-i][3-j] << ' ';
+            } else {
+                cout << input[3-i][3-j] << '\n';
+            }
+        }
+    }
+}
